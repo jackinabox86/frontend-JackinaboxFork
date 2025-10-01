ac07023433623124e0570010a2509351e41775db
Jan <jplacht@gmail.com>
2025-10-01 17:42:45 +0200

    fix(globals): build and runtime variables

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,12 +1,10 @@
-declare const __INDEXEDDB_VERSION__: readonly number;
+/// <reference types="vite/client" />
+
+declare const __INDEXEDDB_VERSION__: number;
 declare const __APP_VERSION__: string;
 
-declare global {
-	interface Window {
-		__APP_CONFIG__?: {
-			POSTHOG_KEY?: string;
-		};
-	}
+interface Window {
+	__APP_CONFIG__?: {
+		POSTHOG_KEY?: string;
+	};
 }
-
-export {};
