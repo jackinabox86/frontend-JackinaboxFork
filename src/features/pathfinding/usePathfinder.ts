0a67ab7e76098c15d5906780de02084d6db825c4
Jan <jplacht@gmail.com>
2026-01-11 11:24:49 +0100

    feat(pathfinding): market precompute, system name get
    #321

--- src/features/pathfinding/usePathfinder.ts
+++ src/features/pathfinding/usePathfinder.ts
@@ -11,8 +11,15 @@ import { AdjecentList, ISystemsJSON } from "./usePathfinder.types";
  * Graph + Cache are initialized only ONCE per session
  */
 
+const systemIdNC1: string = "49b6615d39ccba05752b3be77b2ebf36"; // Moria
+const systemIdAI1: string = "8ecf9670ba070d78cfb5537e8d9f1b6c"; // Antares
+const systemIdCI1: string = "92029ff27c1abe932bd2c61ee4c492c7"; // Benten
+const systemIdIC1: string = "f2f57766ebaca9d69efae41ccf4d8853"; // Hortus
+
+const marketSystemIds = [systemIdNC1, systemIdAI1, systemIdCI1, systemIdIC1];
+
 let initialized: boolean = false;
-let adj: AdjecentList;
+let adjecents: AdjecentList;
 let idToIndex: Map<string, number>;
 let indexToId: string[];
 let parentCache: Map<number, Int32Array>;
@@ -35,8 +42,8 @@ export function usePathfinder() {
 		}
 
 		// create adjecent arrays and fill them
-		adj = new Array(n);
-		for (let i = 0; i < n; i++) adj[i] = [];
+		adjecents = new Array(n);
+		for (let i = 0; i < n; i++) adjecents[i] = [];
 
 		// fyi, connections are always bidirectional
 		for (let i = 0; i < n; i++) {
@@ -47,20 +54,24 @@ export function usePathfinder() {
 				// no mapped index, continue
 				if (j === undefined) continue;
 
-				adj[i].push(j);
+				adjecents[i].push(j);
 
 				// add bidirectional one if i and j differ
-				if (i !== j) adj[j].push(i);
+				if (i !== j) adjecents[j].push(i);
 			}
 		}
 
 		// de-duplicating all neighbors via a SET
 		for (let i = 0; i < n; i++) {
-			adj[i] = Array.from(new Set(adj[i]));
+			adjecents[i] = Array.from(new Set(adjecents[i]));
 		}
 
 		// clear parent cache
 		parentCache = new Map<number, Int32Array>();
+
+		// precompute market BFS
+		_precomputeMarketBFS();
+
 		initialized = true;
 	}
 
@@ -71,13 +82,23 @@ export function usePathfinder() {
 	 * and returns a parent array that allows reconstructing shortest paths
 	 * to all reachable nodes.
 	 *
+	 * @note Important for BFS: for efficiency make use of calculating the
+	 * parents of a node only once. In our context: the optimal way is to
+	 * e.g. calculate the outgoing paths FROM a market to each target instead
+	 * of calculating from a target to the market. By doing this one does only
+	 * computes all paths once and then just pointer walks across the path
+	 * traversal.
+	 *
+	 * BFS creation = O(V+E) -> V = Nodes, E = Connections
+	 * Path traversal = O(Path length)
+	 *
 	 * @param sourceIdx - index of the source node
 	 * @returns Int32Array where parent[i] = index of the previous node
 	 *          along the shortest path from sourceIdx to i
 	 * 			-1 if unreachable
 	 */
 	function bfsParents(sourceIdx: number): Int32Array {
-		const n = adj.length;
+		const n = adjecents.length;
 
 		// -1 = not visited or no parent yet
 		const parent = new Int32Array(n).fill(-1);
@@ -96,7 +117,7 @@ export function usePathfinder() {
 			const cur = queue[head++];
 
 			// iterate through all neighbors of current node
-			for (const nb of adj[cur]) {
+			for (const nb of adjecents[cur]) {
 				// skip if visited
 				if (visited[nb]) continue;
 
@@ -106,11 +127,42 @@ export function usePathfinder() {
 			}
 		}
 
-		// each element now has the index of the previous node
-		// following shortest path from sourceIdx or -1 if unreachable
+		/**
+		 * each element now has the index of the previous node
+		 * following shortest path from sourceIdx or -1 if unreachable
+		 */
 		return parent;
 	}
 
+	/**
+	 * Precomputes BFS for markets and caching the results
+	 * for further uses like in Planet Search
+	 *
+	 * @author jplacht
+	 */
+	function _precomputeMarketBFS() {
+		marketSystemIds.forEach((marketId) => {
+			const marketIdx = idToIndex.get(marketId);
+
+			if (marketIdx === undefined) return;
+
+			if (!parentCache.has(marketIdx)) {
+				parentCache.set(marketIdx, bfsParents(marketIdx));
+			}
+		});
+	}
+
+	/**
+	 * Reconstructs a path between source and target from given
+	 * BFS mapping
+	 *
+	 * @author jplacht
+	 *
+	 * @param {Int32Array} parent BFS
+	 * @param {number} sourceIdx Source node
+	 * @param {number} targetIdx Target node
+	 * @returns {(string[] | null)} Path system ids
+	 */
 	function reconstructPath(
 		parent: Int32Array,
 		sourceIdx: number,
@@ -127,8 +179,10 @@ export function usePathfinder() {
 		const tmp: number[] = [];
 		let cur = targetIdx;
 
-		// return from target back to source over indices,
-		// move to parent node along shortest path
+		/**
+		 * return from target back to source over indices,
+		 * move to parent node along shortest path
+		 */
 		while (cur !== -1) {
 			tmp.push(cur);
 			if (cur === sourceIdx) break;
@@ -141,6 +195,15 @@ export function usePathfinder() {
 		return tmp.map((i) => indexToId[i]);
 	}
 
+	/**
+	 * Calculates the path between two given nodes
+	 *
+	 * @author jplacht
+	 *
+	 * @param sourceId Source Node
+	 * @param targetId Target Node
+	 * @returns Path systems, or null if system ids not found
+	 */
 	function getPathBetween(
 		sourceId: string,
 		targetId: string
@@ -160,25 +223,58 @@ export function usePathfinder() {
 		return reconstructPath(parent, sourceIdx, targetIdx);
 	}
 
-	function getPathBetweenLength(
-		sourceId: string,
-		targetId: string
-	): number | null {
+	/**
+	 * Calculates the path length between given nodes, taking into
+	 * account that the path length in jumps is decreased by one for
+	 * the starting system one jumps out of
+	 *
+	 * @author jplacht
+	 *
+	 * @param {string} sourceId Source Node
+	 * @param {string} targetId Target Node
+	 * @returns {(number} Number of jumps or -1, if unreachable
+	 */
+	function getPathBetweenLength(sourceId: string, targetId: string): number {
 		const path = getPathBetween(sourceId, targetId);
 
-		if (!path) return null;
+		if (!path) return -1;
 
 		/**
 		 * path length minus 1, as we go for jumps and target
-		 * could be in same system which is path lenght of 1
+		 * could be in same system which is path length of 1
 		 */
 
 		return path.length > 0 ? path.length - 1 : 0;
+	}
+
+	/**
+	 * Finds a systems readable name based on its system id
+	 *
+	 * @author jplacht
+	 *
+	 * @param {string} systemId System Id
+	 * @returns {(string | null)} Name (e.g. ZV-759) or null if not found
+	 */
+	function getSystemName(systemId: string): string | null {
+		const systemIdx = idToIndex.get(systemId);
+
+		if (systemIdx === undefined) return null;
+
+		const systemInfo = systemsJson[systemIdx];
+
+		if (systemInfo) return systemInfo.Name;
+		else return null;
 	}
 
 	return {
 		ready,
 		getPathBetween,
 		getPathBetweenLength,
+		getSystemName,
+		// systemid statics
+		systemidNC1: systemIdNC1,
+		systemidAI1: systemIdAI1,
+		systemidCI1: systemIdCI1,
+		systemidIC1: systemIdIC1,
 	};
 }
